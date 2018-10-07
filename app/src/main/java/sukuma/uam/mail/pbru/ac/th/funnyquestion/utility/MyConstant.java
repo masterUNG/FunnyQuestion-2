0f6f32f95a7a3e9a9deea5fa275a2388bfad2c5c
masterUNG <phrombutr@gmail.com>
2018-10-07 13:10:15 +0700

    Answer Spinner Success

--- app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/utility/MyConstant.java
+++ app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/utility/MyConstant.java
@@ -2,6 +2,59 @@ package sukuma.uam.mail.pbru.ac.th.funnyquestion.utility;
 
 public class MyConstant {
 
+    //    Choice Spinner1
+    private String[] choiceSpinner1Strings = new String[]{
+            "1)..............",
+            "To listen to music",
+            "xxx",
+            "To listen music",
+            "xxx",
+            "Listen to music Listen music",
+            "xxx",
+            "Listening to music",
+            "xxx",
+            "To watch movies",
+            "xxx",
+            "Watching movies",
+            "xxx",
+            "To watch movies",
+            "xxx",
+            "Watch movies",
+            "xxx",
+            "Watching a movie",
+            "xxx",
+            "To play games",
+            "xxx",
+            "To play a game",
+            "xxx",
+            "Playing games",
+            "xxx" };
+    private String[] choiceSpinner2Strings = new String[]{
+            "2)..............",
+            "Playing computer games",
+            "To play computer games",
+            "To shop online",
+            "Shopping online",
+            "Online shopping",
+            "Buying things",
+            "xxx",
+            "xxx",
+            "xxx",
+            "xxx"
+    };
+    private String[] choiceSpinner3Strings = new String[]{
+            "3)..............",
+            "To buy things",
+            "Buy a thing",
+            "xxx",
+            "xxx",
+            "xxx",
+            "xxx",
+            "xxx",
+            "xxx",
+            "xxx",
+            "Buy things" };
+
     private String[] unitTitleString = new String[]{
             "Unit1 Computer Users",
             "Unit2",
@@ -10,6 +63,21 @@ public class MyConstant {
             "Unit5",
             "Unit6"
     };
+
+//    Getter
+
+
+    public String[] getChoiceSpinner1Strings() {
+        return choiceSpinner1Strings;
+    }
+
+    public String[] getChoiceSpinner2Strings() {
+        return choiceSpinner2Strings;
+    }
+
+    public String[] getChoiceSpinner3Strings() {
+        return choiceSpinner3Strings;
+    }
 
     public String[] getUnitTitleString() {
 
